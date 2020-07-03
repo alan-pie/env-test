85dee4acb0f09dd829b2d037689cb8a8b07ec502
Alan Savage <65185890+alan-pie@users.noreply.github.com>
2020-07-03 13:03:20 -0400

    initial commit

---
+++ config/webpack/environment.js
@@ -0,0 +1,5 @@
+const { environment } = require('@rails/webpacker')
+
+throw process.env.NODE_ENV
+
+module.exports = environment
---
+++ spec/system/welcome_page_spec.rb
@@ -0,0 +1,7 @@
+require 'rails_helper'
+
+describe 'the welcome page' do
+  it 'loads' do
+    visit ''
+  end
+end
